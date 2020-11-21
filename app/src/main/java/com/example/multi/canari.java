e52582dde63185c1cbd19f2cdb2ace428a22a3e6
d3zile <rpg.maksim@mail.ru>
2020-11-21 13:41:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multi/canari.java
@@ -0,0 +1,14 @@
+package com.example.multi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class canari extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.canari);
+    }
+}
